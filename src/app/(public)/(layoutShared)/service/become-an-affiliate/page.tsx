be0783146588cf580e619ec88b49beaf2283ee5c
anhkhoi1908 <95618205+anhkhoi1908@users.noreply.github.com>
2023-07-07 15:57:42 +0800

    update done 2 pages

--- src/app/(public)/(layoutShared)/service/become-an-affiliate/page.tsx
+++ src/app/(public)/(layoutShared)/service/become-an-affiliate/page.tsx
@@ -2,32 +2,156 @@ import { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import bannerImg from "../../../../../../public/section-img.jpg";
+import sectionImg1 from "../../../../../../public/section1.svg";
+import sectionImg2 from "../../../../../../public/section2.svg";
+import sectionImg3 from "../../../../../../public/section3.svg";
+import sectionWorksImg from "../../../../../../public/section-works.jpg";
+import sectionStoreFront from "../../../../../../public/bg-storefront.webp";
+
 const Page: FC = ({ }) => {
+  const sectionAbout = [
+    {
+      id: 1,
+      img: sectionImg1,
+      title: "Authors",
+      content: "Promote your book via Bookshop to support your writing career and independent bookstores at the same time."
+    },
+    {
+      id: 2,
+      img: sectionImg2,
+      title: "Influencers",
+      content: "Earn when promoting books you love"
+    },
+    {
+      id: 3,
+      img: sectionImg3,
+      title: "Magazines, Publishers, and Web Publications",
+      content: "Earn more on book sales from us than the competition"
+    },
+  ];
+
+  const sectionHowItWorks = [
+    {
+      id: 1,
+      content: "Sign up for an account"
+    },
+    {
+      id: 2,
+      content: "Share links to books you love"
+    },
+    {
+      id: 3,
+      content: "Earn 10% on each sale you create"
+    },
+  ];
+
   return <>
-    <div className="raised-banner">
-      <Link href="">
-        <span>{"$26,658,048.13"}</span>
+    {/* Raised Banner */}
+    <div className="raised-banner text-center flex justify-center items-center my-[1rem]">
+      <Link href="" className="text-[1.8rem]">
+        <span className="text-[#573BA2] text-4xl font-bold mr-[0.5rem]">
+          {"$26,658,048.13"}
+        </span>
         {"raised for local bookstores"}
       </Link>
     </div>
 
-    <div className="banner" id="banner">
-      <div className="flex h-full justify-center flex-col">
-        <h1 className="">
+    {/* Banner */}
+    <div className="banner relative" id="banner">
+      <div className="banner-img h-[45rem] w-full">
+        <Image src={bannerImg} alt="Banner Image" className="h-full w-full" />
+      </div>
+      <div className="flex h-full w-1/3 justify-center flex-col text-white z-10 absolute top-0 ml-[8rem]">
+        <h1 className="font-bold text-7xl">
           {"Become an Affiliate"}
         </h1>
-        <p className="">
+        <p className="text-[1.8rem] py-[2rem] leading-[3rem] w-4/5">
           {"Become an affiliate and earn an industry-leading 10% whenever someone buys a book you share. Please note that your account will need to go through a brief verification process in order for us to onboard your store. We will email you throughout this process to let you know the status of your page."}
         </p>
-        <Link href="" className="inline-block button primary">
-          {"JOIN THE AFFILIATE PROGRAM"}
-        </Link>
+        <div>
+          <Link href="" className="inline-block button primary text-[1.6rem] font-bold bg-[#DE2454] rounded-[4rem] px-10 py-4 hover:scale-105 hover:bg-[#333] duration-300">
+            {"JOIN THE AFFILIATE PROGRAM"}
+          </Link>
+        </div>
       </div>
     </div>
 
-    <section className="grid gap-4 text-center grid-cols-3">
-
+    {/* Section About*/}
+    <section className="px-10rem section-about">
+      <ul className="flex w-4/5 m-auto py-[8rem]">
+        {sectionAbout.map((sectionAboutItem: any, index: number) => (
+          <li key={index} className="w-1/3 flex flex-col justify-items-start items-center text-center">
+            <div>
+              <Image src={sectionAboutItem.img} alt="Section Image" />
+            </div>
+            <h1 className="font-bold text-6xl text-[#2B0F42] my-6 leading-[5rem]">
+              {sectionAboutItem.title}
+            </h1>
+            <p className="text-[1.8rem] w-4/5">{sectionAboutItem.content}</p>
+          </li>
+        ))}
+      </ul>
     </section>
+
+    {/* Section How It Works */}
+    <section className="section-howitworks bg-[#EAE9ED] py-[6rem]">
+      <div className="flex justify-center items-center w-[90%] m-auto">
+        <div className="w-1/2">
+          <Image src={sectionWorksImg} alt="How It Works Image" />
+        </div>
+        <div className="w-1/2 pl-[10rem]">
+          <h1 className="font-bold text-7xl text-[#2B0F42]">
+            {"How It Works:"}
+          </h1>
+          <ul className="my-10">
+            {sectionHowItWorks.map((sectionHowItWorkItem: any, index: number) => (
+              <li key={index} className="flex text-[1.8rem] mb-6 items-center">
+                <nav className="text-white bg-[#573BA3] px-[1.1rem] py-1 rounded-full">
+                  {sectionHowItWorkItem.id}
+                </nav>
+                <nav className="ml-4">{sectionHowItWorkItem.content}</nav>
+              </li>
+            ))}
+          </ul>
+          <h2 className="text-4xl font-bold text-[#2B0F42]">{"Want to learn more? Check our"}</h2>
+          <h3 className="text-4xl font-bold mt-4">
+            <Link href="" className="inline-block text-[#DE2454] hover:underline">{"FAQ"}</Link>
+          </h3>
+        </div>
+      </div>
+    </section>
+
+    {/* Section Become */}
+    <section className="w-1/3 m-auto py-[8rem]">
+      <h1 className="font-bold text-6xl text-[#2B0F42]">
+        {"Why Become An Affiliate"}
+      </h1>
+      <p className="text-[1.8rem] my-8">
+        {"Bookshop is all about giving back to everyone who supports book culture. Authors can double their royalties by selling their books through Bookshop, while supporting local bookstores at the same time! Publications that review books can earn much-needed financial support. Book clubs can earn sustaining funds on each pick, organizations can fundraise, and Instagram influencers can curate their favorite books on their own profile page and earn revenue. All while supporting the entire ecosystem around books! It\'s a win-win-win!"}
+      </p>
+      <nav className="text-[1.8rem]">{"Questions? Email"}
+        <Link href="" className="text-[#DE2854] px-2 hover:underline">{"affiliates@bookshop.org"}</Link>
+        {"to hear more."}
+      </nav>
+    </section>
+
+    {/* Section StoreFront */}
+    <section className="relative section-storefront">
+      <div className="banner-img h-[45rem] w-full">
+        <Image src={sectionStoreFront} alt="Banner Image" className="h-full w-full" />
+      </div>
+      <div className="flex h-full w-1/4 justify-center flex-col text-[#2C293B] z-10 absolute top-0 right-[10%] ml-[8rem]">
+        <h1 className="font-bold text-7xl leading-[6rem] mb-[4rem]">
+          {"Have Your Own Hassle-Free Storefront"}
+        </h1>
+        <div className="text-white">
+          <Link href="" className="inline-block button primary text-[1.6rem] font-bold bg-[#DE2454] rounded-[4rem] px-10 py-4 hover:scale-105 hover:bg-[#333] duration-300">
+            {"BECOME AN AFFILIATE"}
+          </Link>
+        </div>
+      </div>
+    </section >
   </>;
 };
 
--- src/app/(public)/(layoutShared)/service/gift-card/page.tsx
+++ src/app/(public)/(layoutShared)/service/gift-card/page.tsx
@@ -1,9 +1,139 @@
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const Page: FC = ({}) => {
+import giftcardImg from "../../../../../../public/gift-card.webp";
+
+const Page: FC = ({ }) => {
+  const termList = [
+    {
+      id: 1,
+      content: "If you purchase a gift card from a specific store or affiliate on Bookshop, the recipient will be sent to the same store on our site when they click the link to redeem the card, so the store will get credit for any purchases made. However, it is possible for a customer to change to a different shop affiliation if they want to spread their purchases around--the card is like cash, and can be spent how the recipient wants."
+    },
+    {
+      id: 2,
+      content: "Bookshop Gift Cards are only accepted at",
+      link: "https://bookshop.org/",
+      fontStyle: "font-bold",
+    },
+    {
+      id: 3,
+      content: "You can choose a value from $10-$1,000."
+    },
+    {
+      id: 4,
+      content: "All gift cards are digital only and must be sent to a valid email address."
+    },
+    {
+      id: 5,
+      content: "For a personalized touch, include the recipient's name and a personal message on the gift card form. This will appear in the email they receive with the redemption code and link."
+    },
+    {
+      id: 6,
+      content: "If you want the card delivered on a specific day, for example the recipient's birthday, enter the date in our order form and we'll send it to them in the morning of that day."
+    },
+    {
+      id: 7,
+      content: "Bookshop Gift Cards never expire and have no hidden fees."
+    },
+  ];
+
+  const amountList = [{ id: 1, amout: "$10" }, { id: 2, amout: "$15" }, { id: 3, amout: "$25" }, { id: 4, amout: "$50" }, { id: 5, amout: "$100" }, { id: 6, amout: "$250" }, { id: 7, amout: "$500" }, { id: 8, amout: "$1000" },];
+
   return (
     <>
-      <h1>gift card</h1>
+      {/* Raised Banner */}
+      <div className="raised-banner text-center flex justify-center items-center my-[1rem] border-b">
+        <Link href="" className="text-[1.8rem]">
+          <span className="text-[#573BA2] text-4xl font-bold mr-[0.5rem]">
+            {"$26,658,048.13"}
+          </span>
+          {"raised for local bookstores"}
+        </Link>
+      </div>
+
+      {/* Bookshop */}
+      <div className="bookshop w-[70%] mx-auto mb-10" id="bookshop">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-6xl font-bold my-16">
+            {"Bookshop Gift Cards"}
+          </h1>
+          <p className="text-[1.8rem] w-4/5">
+            {"Bookshop digital gift cards are the perfect gift for any avid reader. The card value is added to the recipient's Bookshop account as credit, and never expires. Bookshop digital gift cards can only be redeemed online on Bookshop.org"}
+          </p>
+        </div>
+        <section className="flex justify-between mt-20">
+          <div className="w-[45%]">
+            <Image src={giftcardImg} alt="Gift Card Image" />
+            <ul className="term-list mt-10">
+              {termList.map((termItem: any, index: number) => (
+                <li key={index} className="text-[1.8rem] mb-10 list-item list-disc">
+                  <p style={{ fontStyle: `${termItem.fontStyle}` }}>{termItem.content}</p>
+                  <Link href="" className="font-bold text-[#3078BA] underline">
+                    {termItem.link}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Link href="" className="font-bold text-[#3078BA] underline text-[1.8rem]">
+              {"View the Terms of Service"}
+            </Link>
+            <p className="font-bold text-[1.8rem] mt-10">
+              {"To purchase multiple gift cards please add them one at a time to your cart."}
+            </p>
+          </div>
+          <div className="w-[45%]">
+            <form className="gift_card_order_form">
+              <fieldset>
+                <div className="flex justify-between">
+                  <div className="w-[48%] text-[1.8rem]">
+                    <label>{"To *"}</label>
+                    <input required className="w-full bg-[#F6F5F7] outline-none px-6 py-4 border-2 border-[#E8E8EA] rounded-xl text-[1.6rem]" id="order_gift_card_requirent_email" type="name" name="order[gift_card_recipient_email]" placeholder="Recipients Email" title="Missing a valid email domain"
+                    />
+                  </div>
+                  <div className="w-[48%] text-[1.8rem]">
+                    <label>{"From *"}</label>
+                    <input required className="w-full bg-[#F6F5F7] outline-none px-6 py-4 border-2 border-[#E8E8EA] rounded-xl text-[1.6rem]" id="order_gift_card_sender_email" type="name" name="order[gift_card_sender_email]" placeholder="Your Email" title="Missing a valid email domain"
+                    />
+                  </div>
+                </div>
+                <div className="mt-10">
+                  <label className="text-[1.8rem]">{"Choose An Amount*"}</label>
+                  <ul className="flex flex-wrap mt-4">
+                    {amountList.map((amoutIem: any, index: number) => (
+                      <li key={index} className="text-[2rem] font-bold mr-8 mb-6 flex items-center">
+                        <input type="radio" required className={`w-[2.5rem] h-[2.5rem] !rounded-full relative before:content-[""] before:block before:absolute before:w-[2.6rem] before:h-[2.6rem] before:t-0 before:l-0 before:bg-white checked:before:bg-[#5F44A7] before:border-2 before:border-[#5F44A7] before:rounded-[0.3rem] checked:after:content-[""] checked:after:block checked:after:absolute checked:after:w-[0.6rem] checked:after:h-[1.4rem] checked:after:border checked:after:border-white checked:after:border-r-2 checked:after:border-b-2 checked:after:border-t-0 checked:after:border-l-0 checked:after:top-[0.3rem] checked:after:left-[1rem] checked:after:rotate-45 checked:after:hover:border-white checked:after:duration-100`} name="order[variant_id]" id={amoutIem.id} />
+                        <label htmlFor={amoutIem.id} className="text-[#5F44A7] pl-4">{amoutIem.amout}</label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="mt-10">
+                  <label className="text-[1.8rem] block">{"Add A Message"}</label>
+                  <textarea placeholder="0/250" maxLength={250} className="border-2 border-[#E8E8EA] bg-[#F6F5F7] rounded-xl w-full outline-none text-[1.6rem] p-6 mt-4" id="order_gift_card_message"></textarea>
+                </div>
+                <div className="mt-10">
+                  <label className="text-[1.8rem] block">{"SendDate"}</label>
+                  <input type="date" className="border-2 border-[#E8E8EA] bg-[#F6F5F7] rounded-xl w-full outline-none text-[1.6rem] p-6 mt-4" />
+                </div>
+                <button className="text-[1.6rem] font-bold text-white bg-[#DE2454] rounded-full w-full py-4 mt-20 hover:bg-[#333] hover:scale-105 duration-300">{"ADD TO CART"}</button>
+              </fieldset>
+            </form>
+            <form className="gift_card_redeem_form mt-[10rem]">
+              <h1 className="text-5xl font-bold mb-6">{"Already have a gift card?"}</h1>
+              <label className="text-[1.6rem]">{"Redeem your code here:"}</label>
+              <fieldset className="bg-[#EAE9ED] p-10 mt-6">
+                <label className="text-[1.8rem]">{"Enter Your Code"}</label>
+                <div className="flex mt-6">
+                  <input placeholder="Code #" required type="text" id="gidt_card_code" name="gift_card[code]" className="w-3/5 outline-none rounded-xl text-[1.6rem] px-6" />
+                  <button className="text-[1.6rem] font-bold text-white bg-[#DE2454] rounded-full py-4 px-8 ml-8 hover:bg-[#333] hover:scale-105 duration-300">{"REDEEM"}</button>
+                </div>
+                <p className="text-[1.6rem] text-[#8B868D] mt-10">{"This amount will be added to your store credit."}</p>
+              </fieldset>
+            </form>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
